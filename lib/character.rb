eec75673ecc5e4e723bb0020df81e793ecd450ce
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-26 10:38:27 -0400

    added #name, #fields, @fields to Character, all tests green

--- lib/character.rb
+++ lib/character.rb
@@ -1,11 +1,30 @@
 class Character
-  def initialize(name)
+  def initialize(name, fields)
 
     # Initialization method for Character.
     # Input: name:String.
     # Output: character:Character.
 
     @name = name
+    @fields = fields
+  end
+
+  def name
+
+    # Getter for the character's name.
+    # Input: none.
+    # Output: String.
+
+    @name
+  end
+
+  def fields
+
+    # Getter for the character's optional fields.
+    # Input: none.
+    # Output: Hash.
+
+    @fields
   end
 
 end
