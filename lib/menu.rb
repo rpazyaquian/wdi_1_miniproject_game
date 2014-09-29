ca8afb9435e7d7156f7ec657edae05572108d610
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-28 23:50:26 -0400

    generator working, improving menu specs

---
+++ lib/menu.rb
@@ -0,0 +1,45 @@
+class Menu
+
+  def initialize(options)
+
+    # Creates options for a menu.
+    # Input: Hash of hashes.
+    # Output: Menu.
+
+    @options = options
+
+    # Choices translate the plain text of an option
+    # into its symbol representation.
+
+    @choices = {}
+
+    options.each do |key, value|
+      @choices[value[:text]] = key
+    end
+
+  end
+
+  def options
+
+    # Getter for options.
+    # Input: none.
+    # Output: Hash of hashes.
+
+    @options
+  end
+
+  def choices
+
+    # Getter for choices.
+    # Input: none.
+    # Output: Hash of hashes.
+
+    @choices
+  end
+
+  def add(options)
+
+
+  end
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,98 @@
+require_relative '../lib/menu'
+
+describe Menu do
+
+  before(:all) do
+
+    def hello
+      "Hello!"
+    end
+
+    def goodbye
+      "Goodbye!"
+    end
+
+    options =
+      {
+        :say_hello => {
+          text: "Say hello",
+          action: hello
+        },
+        :say_goodbye => {
+          text: "Say goodbye",
+          action: goodbye
+        }
+      }
+
+    @menu = Menu.new(options)
+  end
+
+  describe "#new" do
+    it "takes a Hash of options and associated data" do
+      expect(@menu.options).to be_a(Hash)
+    end
+  end
+
+  describe "#choices" do
+
+    it "creates choices based on its options" do
+      expect(@menu.choices).to include('Say hello')
+    end
+
+  end
+
+  describe "#add" do
+    it "adds an option to its list of options and actions" do
+
+      new_option = {
+        :new_option => {
+          text: 'New option',
+          action: 'new option!?'
+        }
+      }
+
+      @menu.add(new_option)
+
+      expect(@menu.choices).to include('New option')
+    end
+  end
+
+  describe "#choose" do
+
+    before(:all) do
+
+      def goodnight
+        "Goodnight!"
+      end
+
+      another_option = {
+        :say_goodnight => {
+          text: 'Say goodnight',
+          action: goodnight
+        }
+      }
+
+      @another_menu = Menu.new(another_option)
+    end
+
+    it "invokes actions based on a choice of options" do
+      expect(@menu.choose(:say_hello)).to eq "Hello!"
+    end
+
+    it "invokes other menus as actions" do
+
+      another_menu = {
+        :another_menu => {
+          text: 'Another menu',
+          action: @another_menu
+        }
+      }
+
+      @menu.add(another_menu)
+
+      expect(@menu.choose(:another_menu).choose(:say_goodnight)).to eq 'Goodnight!'
+    end
+
+  end
+
+end
